9d380c605c241e9f1b06f6ec04e8ce8f80522460
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-27 16:55:25 -0400

    using default arguments

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,16 @@
-blockchain = [[1]]
+blockchain = []
 
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
 
 
 add_value(2)
-add_value(.9)
-add_value(10.89)
+add_value(.9, get_last_blockchain_value())
+add_value(10.89, get_last_blockchain_value())
 
 print(blockchain)

3375ec7488070f5b71166621ff255256ed134b0e
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-27 17:16:55 -0400

    using the input function and avoiding repetitive code execution

--- blockchain.py
+++ blockchain.py
@@ -9,8 +9,18 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-add_value(2)
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.9)
-add_value(10.89, get_last_blockchain_value())
+def get_user_input():
+    return float(input('Your transaction amount please: '))
+
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(),
+          transaction_amount=tx_amount)
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
 
 print(blockchain)

7a0271f4356418b005f1d2db94ecdf38d40b519c
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-27 16:34:51 -0400

    adding arguments to functions

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,9 @@
 blockchain = [[1]]
 
-def add_value():
-    blockchain.append([blockchain[-1], 5.3])
+def add_value(transaction_amount):
+    blockchain.append([blockchain[-1], transaction_amount])
     print(blockchain)
 
-add_value()
-add_value()
-add_value()
+add_value(2)
+add_value(.9)
+add_value(10.89)

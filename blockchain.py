1c88020a9bb9f034c92127a8b4b5c7af026f5de9
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-27 17:00:21 -0400

    working with keyword arguments

--- blockchain.py
+++ blockchain.py
@@ -10,7 +10,7 @@ def add_value(transaction_amount, last_transaction=[1]):
 
 
 add_value(2)
-add_value(.9, get_last_blockchain_value())
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.9)
 add_value(10.89, get_last_blockchain_value())
 
 print(blockchain)

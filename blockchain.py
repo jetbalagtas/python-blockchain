e06946411a85ee9345b554f7059c9dc6f52c87e3
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-28 12:50:14 -0400

    return None if blockchain length is 0; if last tx is None, assign 1

--- blockchain.py
+++ blockchain.py
@@ -4,16 +4,20 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction):
     """ Append a new value as well as the last blockchain value, to the blockchain.
 
     Arguments:
         :transaction_amount: The amount that should be added. Required.
         :last_transaction: The last blockchain transaction (default [1]). Optional.
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -35,10 +39,6 @@ def print_blockchain_elements():
         print(block)
 
 
-# Get the first transaction input and add the value to the blockchain
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
 while True:
     print('Please choose')
     print('1: Add a new transaction value')
@@ -47,7 +47,7 @@ while True:
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'q':

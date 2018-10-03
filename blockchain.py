1ce8b5cb9ffe9cf49352e894f7c44046bfef147a
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-03 14:50:41 -0400

    safely add reward_transaction to the block with a local copy of open_transactions

--- blockchain.py
+++ blockchain.py
@@ -73,11 +73,12 @@ def mine_block():
         'recipient': owner,
         'amount': MINING_REWARD
     }
-    open_transactions.append(reward_transaction)
+    copied_transactions = open_transactions[:]
+    copied_transactions.append(reward_transaction)
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
-        'transactions': open_transactions
+        'transactions': copied_transactions
     }
     blockchain.append(block)
     return True

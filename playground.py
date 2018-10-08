beb246a6cf728a4629c2fd325f4cd0991930f799
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-08 19:56:38 -0400

    add comments about unpacking function arguments

--- playground.py
+++ playground.py
@@ -1,4 +1,6 @@
 # unpacking function arguments
+
+# * tells Python to accept an unlimited amount of unnamed arguments and pass them into the function as a tuple
 def unlimited_arguments_tuple(*args):
     print(args)
     for argument in args:
@@ -7,6 +9,7 @@ def unlimited_arguments_tuple(*args):
 unlimited_arguments_tuple(*[1,2,3,4])
 
 
+# ** tells Python to accept and unlimited amount of named arguments and pass them into the function as a dictionary
 def unlimited_arguments_dictionary(**keyword_args):
     print(keyword_args)
     for k, argument in keyword_args.items():

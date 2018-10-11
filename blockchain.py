af2c731c7898f6773cd85e811acfbe998625ec11
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-10 21:56:08 -0400

    add proof of work to the blockchain

--- blockchain.py
+++ blockchain.py
@@ -30,6 +30,22 @@ def hash_block(block):
     return hl.sha256(json.dumps(block).encode()).hexdigest()
 
 
+def valid_proof(transactions, last_hash, proof):
+    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    guess_hash = hl.sha256(guess).hexdigest()
+    print(guess_hash)
+    return guess_hash[0:2] == '00'
+
+
+def proof_of_work():
+    last_block = blockchain[-1]
+    last_hash = hash_block(last_block)
+    proof = 0
+    while valid_proof(open_transactions, last_hash, proof):
+        proof += 1
+    return proof
+
+
 def get_balance(participant):
     """Calculate and return the balance for a participant.
 

8054deacab36ea5954904bd4cf8e6cd6bd1d6363
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-14 18:35:56 -0400

    add error handlers: init blockchain if no blockchain file exists

--- blockchain.py
+++ blockchain.py
@@ -8,15 +8,8 @@ from hash_util import hash_string_256, hash_block
 # The reward given to miners for creating a new block
 MINING_REWARD = 10
 
-# Our starting block for the blockchain
-genesis_block = {
-    'previous_hash': '',
-    'index': 0,
-    'transactions': [],
-    'proof': 100
-}
 # Initializing our (empty) blockchain list
-blockchain = [genesis_block]
+blockchain = []
 # Unhandled transactions
 open_transactions = []
 # We are the owner of this blockchain node, hence this is our identifier (e.g. for sending coins)
@@ -26,11 +19,11 @@ participants = {'Jet'}
 
 def load_data():
     """Initialize blockchain + open transactions data from a file."""
+    global blockchain
+    global open_transactions
     try:
         with open('blockchain.txt', mode='r') as f:
             file_content = f.readlines()
-            global blockchain
-            global open_transactions
             blockchain = json.loads(file_content[0][:-1])
             # We need to convert  the loaded data because Transactions should use OrderedDict
             updated_blockchain = []
@@ -66,11 +59,17 @@ def load_data():
         #     blockchain = file_content['chain']
         #     open_transactions = file_content['ot']
     except IOError:
-        print('File not found!')
-    except ValueError:
-        print('Value error!')
-    except:
-        print('Wildcard!')
+        # Our starting block for the blockchain
+        genesis_block = {
+            'previous_hash': '',
+            'index': 0,
+            'transactions': [],
+            'proof': 100
+        }
+        # Initializing our (empty) blockchain list
+        blockchain = [genesis_block]
+        # Unhandled transactions
+        open_transactions = []
     finally:
         print('Cleanup!')
 
@@ -80,18 +79,20 @@ load_data()
 
 def save_data():
     """Save blockchain + open transactions snapshot to a file."""
-    with open('blockchain.txt', mode='w') as f:
-        f.write(json.dumps(blockchain))
-        f.write('\n')
-        f.write(json.dumps(open_transactions))
-    # USING PICKLE:
-    # with open('blockchain.p', mode='wb') as f:
-    #     save_data = {
-    #         'chain': blockchain,
-    #         'ot': open_transactions
-    #     }
-    #     f.write(pickle.dumps(save_data))
-    print('Saving failed!')
+    try:
+        with open('blockchain.txt', mode='w') as f:
+            f.write(json.dumps(blockchain))
+            f.write('\n')
+            f.write(json.dumps(open_transactions))
+        # USING PICKLE:
+        # with open('blockchain.p', mode='wb') as f:
+        #     save_data = {
+        #         'chain': blockchain,
+        #         'ot': open_transactions
+        #     }
+        #     f.write(pickle.dumps(save_data))
+    except IOError:
+        print('Saving failed!')
 
 
 def valid_proof(transactions, last_hash, proof):

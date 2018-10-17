9f00e5393df430fd7198ae91efc9f73e212e22c2
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-17 16:31:07 -0400

    controlling exports

---
+++ utility/__init__.py
@@ -0,0 +1,6 @@
+# If you want to 'control' which functions you want to export from the bundled modules, this is how to do it.
+# Note that this will only work if the user imports all using the '*' key. The user can still access unexported functions via a named import
+
+# from utility.hash_util import hash_string_256
+
+# __all__ = ['hash_string_256']

ce6844692e73f2f460b76a26d1013290abf0dc95
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-17 21:15:27 -0400

    controlling exports, manipulating open transactions

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,5 +1,5 @@
 # If you want to 'control' which functions you want to export from the bundled modules, this is how to do it.
-# Note that this will only work if the user imports all using the '*' key. The user can still access unexported functions via a named import
+# Note that this will only work if the user imports all using the '*' syntax. The user can still access unexported functions via a named import
 
 # from utility.hash_util import hash_string_256
 

c80a866706bb4a9efde955654e79a613270e5a9b
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-23 16:42:38 -0400

    add the first route

--- node.py
+++ node.py
@@ -1,16 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 from wallet import Wallet
+from blockchain import Blockchain
 
 app = Flask(__name__)
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
 
 @app.route('/', methods=['GET'])
 def get_ui():
     return 'This works!'
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.chain
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

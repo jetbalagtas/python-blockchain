78c0645dfe65f1c9218e25bd87f646c051565dee
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-26 16:01:05 -0400

    node network: add node mgmt methods to the Blockchain

--- blockchain.py
+++ blockchain.py
@@ -35,6 +35,7 @@ class Blockchain:
         self.__open_transactions = []
         self.load_data()
         self.hosting_node = hosting_node_id
+        self.__peer_nodes = set()
 
     
     # def get_chain(self): # used for self.__chain attribute
@@ -70,13 +71,15 @@ class Blockchain:
                     updated_block = Block(block['index'], block['previous_hash'], converted_tx, block['proof'], block['timestamp'])
                     updated_blockchain.append(updated_block)
                 self.chain = updated_blockchain # using self.chain for the setter to kick in and set the chain
-                open_transactions = json.loads(file_content[1])
+                open_transactions = json.loads(file_content[1][:-1])
                 # We need to convert  the loaded data because Transactions should use OrderedDict
                 updated_transactions = []
                 for tx in open_transactions:
                     updated_transaction = Transaction(tx['sender'], tx['recipient'], tx['signature'], tx['amount'])
                     updated_transactions.append(updated_transaction)
                 self.__open_transactions = updated_transactions
+                peer_nodes = json.loads(file_content[2])
+                self.__peer_nodes = set(peer_nodes)
             # USING PICKLE:
             # with open('blockchain.p', mode='rb') as f:
             #     file_content = pickle.loads(f.read())
@@ -99,6 +102,8 @@ class Blockchain:
                 f.write('\n')
                 saveable_tx = [tx.__dict__ for tx in self.__open_transactions]
                 f.write(json.dumps(saveable_tx))
+                f.write('\n')
+                f.write(json.dumps(list(self.__peer_nodes)))
             # USING PICKLE:
             # with open('blockchain.p', mode='wb') as f:
             #     save_data = {
@@ -191,3 +196,23 @@ class Blockchain:
         self.__open_transactions = []
         self.save_data()
         return block
+
+
+    def add_peer_node(self, node):
+        """Adds a new node to the peer node set.
+
+        Arguments:
+            :node: The node URL which should be added.
+        """
+        self.__peer_nodes.add(node)
+        self.save_data()
+
+
+    def remove_peer_node(self, node):
+        """Removes a new node to the peer node set.
+
+        Arguments:
+            :node: The node URL which should be removed.
+        """
+        self.__peer_nodes.discard(node)
+        self.save_data()

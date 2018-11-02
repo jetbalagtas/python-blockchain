3c104c2ccac22bae0ae4154fb61f91edcafa363c
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-02 14:36:54 -0400

    add doc strings and comments

--- blockchain.py
+++ blockchain.py
@@ -227,14 +227,21 @@ class Blockchain:
 
 
     def add_block(self, block):
+        """Add a block which was received via broadcasting to the local blockchain."""
+        # Create a list of transaction objects
         transactions = [Transaction(tx['sender'], tx['recipient'], tx['signature'], tx['amount']) for tx in block['transactions']]
+        # Validate the proof of work of the block and store the result (True or False) in a variable
         proof_is_valid = Verification.valid_proof(transactions[:-1], block['previous_hash'], block['proof'])
+        # Check if previous_hash stored in the block is equal to the local blockchain's last block's hash and store the result in a block
         hashes_match = hash_block(self.chain[-1]) == block['previous_hash']
         if not proof_is_valid or not hashes_match:
             return False
+        # Create a Block object
         converted_block = Block(block['index'], block['previous_hash'], transactions, block['proof'], block['timestamp'])
         self.__chain.append(converted_block)
         stored_transactions = self.__open_transactions[:]
+        # Check which open transactions were included in the received block and remove them
+        # This could be improved by giving each transaction an ID that would uniquely identify it
         for itx in block['transactions']:
             for opentx in stored_transactions:
                 if opentx.sender == itx['sender'] and opentx.recipient == itx['recipient'] and opentx.amount == itx['amount'] and opentx.signature == itx['signature']:
@@ -247,22 +254,29 @@ class Blockchain:
 
 
     def resolve(self):
+        """Checks all peer nodes' blockchains and replaces the local one with longer valid ones."""
+        # Initialize the winner chain with the local chain
         winner_chain = self.chain
         replace = False
         for node in self.__peer_nodes:
             url = 'http://{}/chain'.format(node)
             try:
+                # Send a request and store the response
                 response = requests.get(url)
+                # Retrieve the JSON data as a dictionary
                 node_chain = response.json()
+                # Convert the dictionary list to a list of block AND transaction objects
                 node_chain = [Block(block['index'], block['previous_hash'], [Transaction(tx['sender'], tx['recipient'], tx['signature'], tx['amount']) for tx in block['transactions']], block['proof'], block['timestamp']) for block in node_chain]
                 node_chain_length = len(node_chain)
                 local_chain_length = len(winner_chain)
+                # Store the received chain as the current winner chain if it's longer AND valid
                 if node_chain_length > local_chain_length and Verification.verify_chain(node_chain):
                     winner_chain = node_chain
                     replace = True
             except requests.exceptions.ConnectionError:
                 continue
         self.resolve_conflicts = False
+        # Replace the local chain with the winner chain
         self.chain = winner_chain
         if replace:
             self.__open_transactions = []
